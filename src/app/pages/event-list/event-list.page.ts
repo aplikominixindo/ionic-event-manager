ae0c656e0094274fbdbdc5fc3aaf09e730817c45
Yusuf Rizal <yusufrizalh@gmail.com>
2019-02-27 14:13:26 +0700

    New Commit

--- src/app/pages/event-list/event-list.page.ts
+++ src/app/pages/event-list/event-list.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EventService } from '../../services/event/event.service';
 
 @Component({
   selector: 'app-event-list',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event-list.page.scss'],
 })
 export class EventListPage implements OnInit {
+  public eventList: Array<any>;
 
-  constructor() { }
+  constructor(private eventService: EventService) {}
 
   ngOnInit() {
+    this.eventService
+      .getEventList()
+      .get()
+      .then(eventListSnapshot => {
+        this.eventList = [];
+        eventListSnapshot.forEach(snapshot => {
+          this.eventList.push({
+            id: snapshot.id,
+            name: snapshot.data().name,
+            price: snapshot.data().price,
+            date: snapshot.data().date
+          });
+          return false;
+        });
+      });
   }
 
 }
