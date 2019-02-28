9dfcd4664ddd1d4596a5841c1d8c0583f6a1e053
Yusuf Rizal <yusufrizalh@gmail.com>
2019-02-28 10:47:22 +0700

    New commit

--- src/app/pages/reset-password/reset-password.page.ts
+++ src/app/pages/reset-password/reset-password.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/user/auth.service';
+import { AlertController } from '@ionic/angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-reset-password',
@@ -6,10 +10,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reset-password.page.scss'],
 })
 export class ResetPasswordPage implements OnInit {
+  public resetPasswordForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private alertCtrl: AlertController,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
+    this.resetPasswordForm = this.formBuilder.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+    });
+  }
 
   ngOnInit() {
+  }
+
+  resetPassword(resetPasswordForm: FormGroup): void {
+    if (!resetPasswordForm.valid) {
+      console.log('Form is not valid, error: ', resetPasswordForm.value);
+    } else {
+      const email: string = resetPasswordForm.value.email;
+      this.authService.resetPassword(email).then(async () => {
+        const alert = await this.alertCtrl.create({
+          message: 'Check your email for password reset.',
+          buttons: [
+            {
+              text: 'OK',
+              role: 'cancel',
+              handler: () => {
+                this.router.navigateByUrl('login');
+              },
+            },
+          ],
+        });
+        await alert.present();
+      },
+      async error => {
+        const errorAlert = await this.alertCtrl.create({
+          message: error.message,
+          buttons: [{ text: 'OK', role: 'cancel' }],
+        });
+        await errorAlert.present();
+      });
+    }
   }
 
 }
